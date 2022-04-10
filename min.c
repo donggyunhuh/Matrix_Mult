c4fb279348899466f4bb3b14b2d21cf5b78c3e0d
jaebeom02 <gsxxx007@naver.com>
2022-04-11 00:02:40 +0900

    김재범 min 수정완료

--- min.c
+++ min.c
@@ -1,24 +1,35 @@
 //
 // Created by j2bom on 2022-04-09.
 //
-#include <math.h>
+
+//2022-04-10 최종수정.
+//최대한 책에 있는 알고리즘에 맞춰 구현해보도록 노력했습니다.
+//main함수는 고려안하고 한 점 이해부탁드립니다.
 
 int arr[100][2]; // 2차원 행렬 선언
 int d[100][100]; // 최솟값 저장
 
-int min(int i, int j)
+
+
+int min(int n)
 {
-    int n;
-    for(i = 1; i <= n; i++) {
-        j = i + 1;
-        if(i == j)
-            return 0;
-        else{
-            d[i][j] = 999999999;
-            for(int k = i; k < j; k++)  {
-                d[i][j] = min(d[i][j], d[i][k] + d[k + 1][j] + (arr[i][0] * arr[k][1] * arr[j][1]));
-        }
+    int i, j;
+    for (int diag = 1; diag <= n; diag++) {
+        for (i = 1; i <= n; i++) {
+            j = diag + i;
+            if (i == j)
+                return 0;
+            else {
+                d[i][j] = 999999999;
+                for (int k = i; k < j; k++) {
+                    int temp = d[i][k] + d[k + 1][j] + arr[i][0] * arr[k][1] * arr[j][1];
+
+                    if (temp < d[i][j])
+                        d[i][j] = temp;
+                }
+            }
         }
     }
-    return d[0][n-1];
+
+    return d[1][n];
 }
