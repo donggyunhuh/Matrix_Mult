8e89999a6faa4a2af74267941c573de771d726a2
donggyunHeo <yje07052@gmail.com>
2022-04-11 11:42:37 +0900

    final

--- matrix-mult.c
+++ matrix-mult.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
 
-int arr[100][2]; 
-int d[100][100];
+int arr[100][2]; // 2차원 행렬 선언
+int d[100][100]; // 최솟값 저장
 
 int min(int i, int j)
 {
 
-    if (i == j) 
+    if (i == j) // 같은행렬까리 곱하면 0
         return 0;
 
-    int val = 999999999; 
+    int val = 999999999; // 최솟값을 구하려면 나올 수 있는 가장 큰 수와 비교했을 때 더 커야함
     for (int k = i; k < j; k++)
     {
-        int temp = d[i][k] + d[k + 1][j] + arr[i][0] * arr[k][1] * arr[j][1]; 
+        int temp = d[i][k] + d[k + 1][j] + arr[i][0] * arr[k][1] * arr[j][1]; // arr[i][0] 행의 수(세로), arr[k][1] 열의 수 arr[j][1] 마지막 행렬의 열의 수, temp값 설정
 
         if (temp < val)
-            val = temp; 
+            val = temp; // 최솟값으로 val 변경 후 저장
         else
-            val = val; 
+            val = val; // val 값 그대로 유지
     }
     return val;
 }
 
-<<<<<<< HEAD
-
-
-int makeArray(int n) 
-{
-    for (int i = 0; i < n; i++) {
-=======
 int makeArray(int n) //행렬만들기
 {
     for (int i = 0; i < n; i++)
     {
->>>>>>> 05f42d037cc7ac0acd657b89abf0d30d2f081143
         printf("arr[%d][0]: ", i);
         scanf("%d", &arr[i][0]);
         printf("arr[%d][0]: ", i);
         scanf("%d", &arr[i][1]);
         printf("\n");
-    }
+        }
 
     return arr[n][2];
 }
@@ -48,58 +40,35 @@ int main()
 {
     int n, minmult;
 
-<<<<<<< HEAD
-    printf("d" ");
-=======
     printf("행렬의 갯수: ");
->>>>>>> 05f42d037cc7ac0acd657b89abf0d30d2f081143
     scanf("%d", &n);
 
     makeArray(n);
 
-<<<<<<< HEAD
-    for (int i = 0; i < n; i++) {
-        int k = 0;
-        for (int j = i; j <= n; j++) {
-=======
     for (int i = 0; i < n; i++)
     {
         int k = 0;
         for (int j = i; j < n; j++)
         {
->>>>>>> 05f42d037cc7ac0acd657b89abf0d30d2f081143
             d[k][j] = min(k, j);
             k++;
         }
     }
 
-<<<<<<< HEAD
-    printf("============d[i][j]============\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-=======
     printf("=========d[i][j]============\n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
->>>>>>> 05f42d037cc7ac0acd657b89abf0d30d2f081143
             printf(" %d ", d[i][j]);
         }
         printf("\n");
     }
-<<<<<<< HEAD
-
-    minmult = d[0][n - 1];
-
-    printf("占쌍쇽옙 占쏙옙占쏙옙 횟占쏙옙: %d", minmult);
-=======
     printf("\n");
 
     minmult = d[0][n - 1];
 
     printf("최소 연산 횟수: %d", minmult);
->>>>>>> 05f42d037cc7ac0acd657b89abf0d30d2f081143
 
     return 0;
 }
