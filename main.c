671190979510a13544d650e5b7070058cc3dd77a
Lee Subin <nnibuseel@naver.com>
2022-04-11 11:47:15 +0900

    main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 int arr[100][2];
 int d[100][100];
 
-int makeArray(int n)
+int makeArray(int n)   //행렬 만들기
 {
     for (int i = 0; i < n; i++) {
         printf("arr[%d][0]: ", i);
@@ -31,7 +31,7 @@ int main()
         }
     }
 
-    printf("============d[i][j]============\n");
+    printf("============d[i][j]============\n");  //연산 횟수 나타내기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf(" %d ", d[i][j]);
